5b8b71d822be104d71dd014a7ba27e822cc583ce
Daniel Ramirez Castano <dm.ramirez@globant.com>
2021-12-29 15:48:10 -0500

    Fixed some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BasicExampleComponent } from './components/basic-example/basic-example.component';
 import { ComponentAsTemplateComponent } from './components/component-as-template/component-as-template.component';
-import { CardComponent } from './components/card/card.component';
+import { CardComponent } from './shared/card/card.component';
 import { TemplateAsInputComponent } from './components/template-as-input/template-as-input.component';
 import { ParentComponent } from './components/template-as-input/parent/parent.component';
 import { RenderingBasedConditionComponent } from './components/rendering-based-condition/rendering-based-condition.component';
